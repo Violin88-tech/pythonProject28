26a84fbc700afe312d1f71b2d34261732cece22d
Viktoria <48157182+violin88@users.noreply.github.com>
2024-01-01 22:10:16 +0400

    change hw

--- tests/test_shop.py
+++ tests/test_shop.py
@@ -1,103 +1,47 @@
+"""
+Протестируйте классы из модуля homework/models.py
+"""
+import pytest
 
-from dataclasses import dataclass
-from typing import Dict
+from tests.models import Product, Cart
 
-class Product:
+
+@pytest.fixture
+def product():
+    return Product("book", 100, "This is a book", 1000)
+
+def cart():
+    return Cart()
+
+class TestProducts:
     """
-    Класс продукта
+    Тестовый класс - это способ группировки ваших тестов по какой-то тематике
+    Например, текущий класс группирует тесты на класс Product
     """
-    name: str
-    price: float
-    description: str
-    quantity: int
 
-    def __init__(self, name, price, description, quantity):
-        self.name = name
-        self.price = price
-        self.description = description
-        self.quantity = quantity
-
-    def check_quantity(self, quantity) -> bool:
-        """
-        TODO Верните True если количество продукта больше или равно запрашиваемому
-            и False в обратном случае
-        """
-        if self.quantity >= quantity:
-            return True
-        else:
-            return False
+    def test_product_check_quantity(self, product):
+        # TODO напишите проверки на метод check_quantity
+        assert product.check_quantity(1) == True
+        assert product.check_quantity(1000) == True
+        assert product.check_quantity(1001) is False
 
 
-    def buy(self, quantity):
-        """
-        TODO реализуйте метод покупки
-            Проверьте количество продукта используя метод check_quantity
-            Если продуктов не хватает, то выбросите исключение ValueError
-        """
-        if self.quantity(quantity):
-            return 'Success'
-        else:
-            raise ValueError
+    def test_product_buy(self, product):
+        # TODO напишите проверки на метод buy
+        product.buy(20)
+        assert product.quantity == 980
 
-    def __hash__(self):
-        return hash(self.name + self.description)
+    def test_product_buy_more_than_available(self, product):
+        # TODO напишите проверки на метод buy,
+        #  которые ожидают ошибку ValueError при попытке купить больше, чем есть в наличии
+        with pytest.raises(ValueError):
+            product.buy(1001)
 
 
-class Cart:
+class TestCart:
     """
-    Класс корзины. В нем хранятся продукты, которые пользователь хочет купить.
-    TODO реализуйте все методы класса
+    TODO Напишите тесты на методы класса Cart
+        На каждый метод у вас должен получиться отдельный тест
+        На некоторые методы у вас может быть несколько тестов.
+        Например, негативные тесты, ожидающие ошибку (используйте pytest.raises, чтобы проверить это)
     """
-    products: Dict[Product, int]
-
-    # Словарь продуктов и их количество в корзине
-    products: dict[Product, int]
-
-    def __init__(self):
-        # По-умолчанию корзина пустая
-        self.products = {}
-
-    def add_product(self, product: Product, buy_count=1):
-        """
-        Метод добавления продукта в корзину.
-        Если продукт уже есть в корзине, то увеличиваем количество
-        """
-        if product in self.products:
-            self.products[product] += buy_count
-        else:
-            self.products[product] = buy_count
-        return self.products
-
-    def remove_product(self, product: Product, remove_count=None):
-        """
-        Метод удаления продукта из корзины.
-        Если remove_count не передан, то удаляется вся позиция
-        Если remove_count больше, чем количество продуктов в позиции, то удаляется вся позиция
-        """
-        if product in self.products:
-            if remove_count is None:
-                self.products.pop(product)
-            else:
-                self.products[product] -= remove_count
-                if self.products[product] <= 0:
-                    self.products.pop(product)
-        return self.products
-
-    def clear(self):
-        raise NotImplementedError
-
-    def get_total_price(self) -> float:
-        raise NotImplementedError
-
-    def buy(self):
-        """
-        Метод покупки.
-        Учтите, что товаров может не хватать на складе.
-        В этом случае нужно выбросить исключение ValueError
-        """
-        for product in self.products:
-            if product.check_quantity(self.products[product]):
-                product.quantity -= self.products[product]
-            else:
-                raise ValueError
-        return self.clear()
